bc8b1614e4cd3d664396db07f330200caaeb1503
Ankita248 <102206335+Ankita248@users.noreply.github.com>
2022-07-12 21:18:10 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,7 @@
+z=int(input("Enter a number"))
+if z==0:
+    print("It is neither even nor odd")
+elif z%2==0:
+    print("It is even")
+else:
+    print("It is odd")
---
+++ main1.py
@@ -0,0 +1,5 @@
+i=int(input("enter a number"))
+print(i)
+while i<10:
+    i=i+1
+    print (i)
